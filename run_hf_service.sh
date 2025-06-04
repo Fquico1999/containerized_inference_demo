36dfcfb3bc166b6e54e0218e22e279e986ef4f81
Fquico1999 <fquico1999@gmail.com>
2025-06-03 19:31:17 -0700

    Run prometheus service detached so not to block inference service startup

--- run_hf_service.sh
+++ run_hf_service.sh
@@ -6,6 +6,12 @@ mkdir -p ./prometheus_metrics && chmod 777 ./prometheus_metrics/
 # Create docker network to host both inference and Prometheus container
 docker network create hf_inference_network
 
+# Run Prometheus container
+docker run -d --rm -p 9090:9090 \
+    --network hf_inference_network \
+    -v "$(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml" \
+    --name prometheus_server prom/prometheus
+
 # Run inference container
 docker run --rm -p 8000:8000 \
     --network hf_inference_network \
@@ -13,9 +19,3 @@ docker run --rm -p 8000:8000 \
     -v "$(pwd)/hf_cache:/app/.cache/huggingface" \
     -v "$(pwd)/prometheus_metrics:/app/prometheus_metrics" \
     --name hf_service hf-inference-service
-
-# Run Prometheus container
-docker run --rm -p 9090:9090 \
-    --network hf_inference_network \
-    -v "${pwd}/prometheus.yml:/etc/prometheus/prometheus.yml" \
-    --name prometheus_server prom/prometheus
