8f0017b82d6737729771a120b6864a7e73198f8f
Fquico1999 <fquico1999@gmail.com>
2025-06-03 19:25:21 -0700

    Creating docker network to host both inference container and prometheus server

---
+++ run_hf_service.sh
@@ -0,0 +1,21 @@
+#/usr/bin/bash
+
+# Create cache folders
+mkdir -p ./hf_cache
+mkdir -p ./prometheus_metrics && chmod 777 ./prometheus_metrics/
+# Create docker network to host both inference and Prometheus container
+docker network create hf_inference_network
+
+# Run inference container
+docker run --rm -p 8000:8000 \
+    --network hf_inference_network \
+    -v "$(pwd)/app:/app/app" \
+    -v "$(pwd)/hf_cache:/app/.cache/huggingface" \
+    -v "$(pwd)/prometheus_metrics:/app/prometheus_metrics" \
+    --name hf_service hf-inference-service
+
+# Run Prometheus container
+docker run --rm -p 9090:9090 \
+    --network hf_inference_network \
+    -v "${pwd}/prometheus.yml:/etc/prometheus/prometheus.yml" \
+    --name prometheus_server prom/prometheus
